78fcd768e950eaa4d650261a5d20578ae7a271fc
Nicolas Herrera <herreranicoo17@gmail.com>
2023-03-12 19:08:23 -0300

    Agregue validaciones y realice lo solicitado en el trabajo practico

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,8 +1,16 @@
-let numLimite = parseInt(prompt("Ingrese un número no mayor a 50"));
+let numLimite = parseInt(prompt("Ingrese un número entre 1 y 50"));
 
-for (let num = 0; numLimite > 0; numLimite--) {
-  for (let numVueltas = 0; numVueltas < numLimite; numVueltas++) {
-    document.write(numLimite);
+if (numLimite < 1 || numLimite > 50) {
+  alert(
+    "El número ingresado esta fuera del rango. Recuerda debe ser un número entre 1 y 50."
+  );
+} else if (isNaN(numLimite)) {
+  alert("Debes ingresar un número valido");
+} else {
+  for (let num = 0; numLimite > 0; numLimite--) {
+    for (let numVueltas = 0; numVueltas < numLimite; numVueltas++) {
+      document.write(numLimite);
+    }
+    document.write("<br>");
   }
-  document.write("<br>")
 }
