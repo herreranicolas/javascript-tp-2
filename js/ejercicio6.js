3f3d80026c843f4ee6d39d75a384fa94b64063bb
Nicolas Herrera <herreranicoo17@gmail.com>
2023-03-12 18:36:45 -0300

    Agregue el archivo ejercicio6.js y resolví lo solicitado en el trabajo practico

---
+++ js/ejercicio6.js
@@ -0,0 +1,6 @@
+for (let numero = 1; numero <= 30; numero++) {
+  for (let numVueltas = 1; numVueltas < numero + 1; numVueltas++) {
+    document.write(`${numero}`);
+  }
+  document.write("<br>");
+}
